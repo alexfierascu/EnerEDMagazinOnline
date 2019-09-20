51c470e58f0c7d03a597eec68e82be69a9bc53d3
Alex Fierascu <ioan-alexandru.fierascu@externals.adidas.com>
2019-09-20 16:10:37 +0300

    comment actuator dependency for the moment, will use it when needed

---
+++ src/main/java/com/alexf/eneredmagazinonline/EneredMagazinOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.alexf.eneredmagazinonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EneredMagazinOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EneredMagazinOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexf/eneredmagazinonline/EneredMagazinOnlineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexf.eneredmagazinonline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EneredMagazinOnlineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
